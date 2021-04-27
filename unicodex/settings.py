cefdb8789ff0add6ba25e8ce537e78df981adcc4
harshalgalgale <h.galgale@gmail.com>
2021-04-27 05:57:22 +0100

    Added accounts app

--- unicodex/settings.py
+++ unicodex/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
 
     # Application definition
     "unicodex",
+    'accounts',
     'students',
     'core',
     'committee',
@@ -172,7 +173,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
+AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
